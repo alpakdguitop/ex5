543dc1ac2cb5fcc50c57459fdf67e1cc2a6e855e
alpakdguitop <93211603+alpakdguitop@users.noreply.github.com>
2021-12-16 13:32:59 +0100

    Add files via upload

---
+++ ex5/escripio.js
@@ -0,0 +1,26 @@
+/*
+	Exemplo de javascript
+*/
+alert("Sal de aquí corriendo");
+ /*var nome;
+    nome = prompt("Dime un numero?   ");
+    alert("Guau... que numero máis interesante el " + nome);
+    alert("es un gran numero el " + nome);
+    alert("Moita utilidade el " + nome);
+    alert("Es mi numero favorito el " + nome);
+    alert("e... " + nome + ", intel!");
+    */
+var papa;
+papa = prompt("Escoge un numero");
+alert("la solucion es:" + papa * 1000);
+var com;
+com = prompt("Cuantos kilometros quieres pasar a metros");
+alert("la solucion es igual a:" + com * 1000 + "metros");
+var com;
+com = prompt("Cuantos metros quieres pasar a kilometros");
+alert("la solucion es igual a:" + com * 0.001 + "KM");
+var com;
+com = prompt("Cuantos dolares quieres pasar a euros");
+alert("la solucion es igual a:" + com * 1.13 + "euros");
+com = prompt("Cuantos euros quieres pasar a dolares");
+alert("la solucion es igual a:" + com / 1.13 + "Dolares");
